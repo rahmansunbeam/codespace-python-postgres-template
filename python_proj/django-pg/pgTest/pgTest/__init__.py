9ab666670d29ebba4eeb7e03a945ea8f410509d2
rahmansunbeam <blu_sr@yahoo.com>
2022-06-28 09:25:16 +0000

    commit

---
+++ python_proj/django-pg/pgTest/pgTest/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+__author__ = 'Sunbeam Rahman'
+__date__   = '27/06/2022 14:13:42'
+__email__  = 'sunbeam.rahman@live.com'
+
+## -------------------------------- ## 
+
