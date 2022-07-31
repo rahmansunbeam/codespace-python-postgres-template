ed1c4e29c3392d9d8493edf609160e97da574a9f
rahmansunbeam <blu_sr@yahoo.com>
2022-07-31 08:46:58 +0000

    renamed:    python_proj/.gitignore -> .gitignore 	modified:   python_proj/test.py 	modified:   src/main.rs 	modified:   src/target/.rustc_info.json 	modified:   src/target/debug/src

---
+++ python_proj/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sunbeam Rahman'
+__date__   = '18/07/2022 20:51:50'
+__email__  = 'sunbeam.rahman@live.com'
+
+## -------------------------------- ##
+
